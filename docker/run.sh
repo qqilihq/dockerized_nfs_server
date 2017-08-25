18b447ca7e2ca0c87d95b14751fc9507f46ac925
Philipp Katz <philipp@philippkatz.de>
2017-08-25 14:50:56 +0200

    Add EXPORT_OPTIONS environment variable to allow overriding the config

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,9 @@ set -e
 
 export_base="/exports/"
 
+# (optionally) allow to specify custom export options through environment
+export_options=${EXPORT_OPTIONS:-rw,sync,insecure,no_subtree_check,no_root_squash}
+
 ### Handle `docker stop` for graceful shutdown
 function shutdown {
     echo "- Shutting down nfs-server.."
@@ -15,7 +18,7 @@ trap "shutdown" SIGTERM
 ####
 
 echo "Export points:"
-echo "$export_base *(rw,sync,insecure,fsid=0,no_subtree_check,no_root_squash)" | tee /etc/exports
+echo "$export_base *($export_options,fsid=0)" | tee /etc/exports
 
 read -a exports <<< "${@}"
 for export in "${exports[@]}"; do
@@ -23,7 +26,7 @@ for export in "${exports[@]}"; do
     src="$export_base$src"
     mkdir -p $src
     chmod 777 $src
-    echo "$src *(rw,sync,insecure,no_subtree_check,no_root_squash)" | tee -a /etc/exports
+    echo "$src *($export_options)" | tee -a /etc/exports
 done
 
 echo -e "\n- Initializing nfs server.."
